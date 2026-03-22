1c55e4ead0f84ae10aed770fe86e2d35a3794436
Mahliyo <gasandigital13@gmail.com>
2026-03-22 17:40:03 +0500

    ifelse katta sonni top

--- main.js
+++ main.js
@@ -24,7 +24,7 @@
  let a = +prompt("a sonini kiriting");
  let b = +prompt("b sonini kiriting");
  let c = +prompt("c sonini kiriting");
- function katta(check){
+ function check(check){
      h = Math.max(a, b, c)
      console.log(h);
      
