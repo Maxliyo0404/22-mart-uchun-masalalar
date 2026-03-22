8a848df1e60fb82cb1671164aedcf278cc4d617f
Mahliyo <gasandigital13@gmail.com>
2026-03-22 16:48:27 +0500

    masala

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // let a = 12
 // console.log(typeof a);
-let parta = 30;
-let broken = 5
-let brokenleft = 2
-let lastbroken = 2
-console.log(parta * 4 - broken * 1 -brokenleft * 2 -lastbroken * 3);
+// let parta = 30;
+// let broken = 5
+// let brokenleft = 2
+// let lastbroken = 2
+// console.log(parta * 4 - broken * 1 -brokenleft * 2 -lastbroken * 3);
