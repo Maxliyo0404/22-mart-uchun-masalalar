78ec922ccd2fd755f8c432cbb13acdb0057a60d0
Mahliyo <gasandigital13@gmail.com>
2026-03-22 16:46:26 +0500

    masala

--- main.js
+++ main.js
@@ -3,4 +3,5 @@
 let parta = 30;
 let broken = 5
 let brokenleft = 2
-console.log(parta * 4 - broken * 1 -brokenleft * 2);
+let lastbroken = 2
+console.log(parta * 4 - broken * 1 -brokenleft * 2 -lastbroken * 3);
